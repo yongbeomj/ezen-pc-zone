bf4616b869d4e876b401a7009f5cfc4f6afbc5d0
swh7j <swh_77@naver.com>
2021-11-11 10:59:46 +0900

    12

---
+++ Admin/src/Controller/SalesController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SalesController {
+	
+	   @FXML
+	    private Button btnback;
+	    @FXML
+	    void back(ActionEvent event) {
+	    	SystemController.getinstance().loadpage("a_system1");
+	    }
+	    
+	    
+	
+
+}
